68f4031b10a282f66bcde7261cc5a7b089fa420d
fangzheng <officefangzheng@fake.com>
2014-09-19 12:01:28 +0800

    after book's listing 19.7

--- src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,6 +17,7 @@ public class CrimeCameraFragment extends Fragment {
 	private SurfaceView mSurfaceView;
 	
 	@Override
+	@SuppressWarnings("deprecation")
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime_camera, container, false);
@@ -29,7 +31,8 @@ public class CrimeCameraFragment extends Fragment {
 		});
 		
 		mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
-		
+		SurfaceHolder holder = mSurfaceView.getHolder();
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		return v;
 	}
 	
