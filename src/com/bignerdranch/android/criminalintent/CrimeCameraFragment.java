16334de05fcfbac631405b45f97c5a945a0e902e
officelenovo <officelenovo@fake.com>
2014-09-19 11:53:49 +0800

    after book's listing 19.6

--- src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -30,5 +31,24 @@ public class CrimeCameraFragment extends Fragment {
 		mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
 		
 		return v;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD) {
+			mCamera = Camera.open(0);
+		} else {
+			mCamera = Camera.open();
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mCamera != null) {
+			mCamera.release();
+			mCamera = null;
+		}
 	}
 }
