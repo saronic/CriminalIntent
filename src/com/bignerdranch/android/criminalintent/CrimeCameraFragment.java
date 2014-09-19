d41c5ff29b3d5fa53be60f1a8f23c75ec1388775
officelenovo <officelenovo@fake.com>
2014-09-19 12:32:23 +0800

    after listing 19.10

--- src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -1,11 +1,18 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class CrimeCameraFragment extends Fragment {
+	protected static final String TAG = "CrimeCameraFragment";
 	private Camera mCamera;
 	private SurfaceView mSurfaceView;
 	
@@ -33,6 +41,42 @@ public class CrimeCameraFragment extends Fragment {
 		mSurfaceView = (SurfaceView) v.findViewById(R.id.crime_camera_surfaceView);
 		SurfaceHolder holder = mSurfaceView.getHolder();
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		holder.addCallback(new Callback() {
+			
+			public void surfaceDestroyed(SurfaceHolder holder) {
+				if (mCamera != null) {
+					mCamera.stopPreview();
+				}
+				
+			}
+			
+			public void surfaceCreated(SurfaceHolder holder) {
+				try {
+					if (mCamera != null) {
+						mCamera.setPreviewDisplay(holder);
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "Error setting up preview display",e);
+				}
+				
+			}
+			
+			public void surfaceChanged(SurfaceHolder holder, int forart, int w, int h) {
+				if (mCamera == null) return;
+				Camera.Parameters parameters = mCamera.getParameters();
+				Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
+				parameters.setPreviewSize(s.width, s.height);
+				mCamera.setParameters(parameters);
+				try {
+					
+				} catch (Exception e) {
+					Log.e(TAG, "Could not start preview", e);
+					mCamera.release();
+					mCamera = null;
+				}
+				
+			}
+		});
 		return v;
 	}
 	
@@ -53,5 +97,18 @@ public class CrimeCameraFragment extends Fragment {
 			mCamera.release();
 			mCamera = null;
 		}
+	}
+	
+	private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
+		Size bestSize = sizes.get(0);
+		int largeArea = bestSize.width * bestSize.height;
+		for (Size s : sizes) {
+			int area = s.width * s.height;
+			if (area > largeArea) {
+				bestSize = s;
+				largeArea = area;
+			}
+		}
+		return bestSize;
 	}
 }
